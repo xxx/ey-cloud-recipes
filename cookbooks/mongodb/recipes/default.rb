55a406d4f7985c76032f68d0c04250bcd87b2876
mpd <michael@tennmandigital.com>
2010-04-05 12:10:15 -0700

    * update MongoDB to 1.4.0
    * Detect 32 bit instances and install the correct version of MongoDB in those cases.

--- cookbooks/mongodb/recipes/default.rb
+++ cookbooks/mongodb/recipes/default.rb
@@ -22,11 +22,24 @@ directory "/data/slave" do
 end
   
 execute "install-mongodb" do
+  # http://aws.amazon.com/ec2/instance-types/ for sizes
+  size = `curl -s http://instance-data.ec2.internal/latest/meta-data/instance-type`
+
+  # small instances and high CPU medium instances have 32 bit architecture.
+  # MongoDB has a max data-size limit on 32 bit architectures of ~2.5 GB.
+  if size == 'm1.small' or size == 'c1.medium'
+    mongo_root = "mongodb-linux-i686-1.4.0"
+  else
+    mongo_root = "mongodb-linux-x86_64-1.4.0"
+  end
+
+  mongo_file = "#{mongo_root}.tgz"
+
   command %Q{
-    curl -O http://downloads.mongodb.org/linux/mongodb-linux-x86_64-1.0.0.tgz &&
-    tar zxvf mongodb-linux-x86_64-1.0.0.tgz &&
-    mv mongodb-linux-x86_64-1.0.0 /usr/local/mongodb &&
-    rm mongodb-linux-x86_64-1.0.0.tgz
+    curl -O http://downloads.mongodb.org/linux/#{mongo_file} &&
+    tar zxvf #{mongo_file} &&
+    mv #{mongo_root} /usr/local/mongodb &&
+    rm #{mongo_file}
   }
   not_if { File.directory?('/usr/local/mongodb') }
 end
